771450ea646faa8584083909511cb87410dd99c1
bryanbill <emobrya@gmail.com>
2023-02-23 11:25:36 +0300

    ft:expanded widget - adds flex factor, defaults to 1

--- lib/src/components/expanded.dart
+++ lib/src/components/expanded.dart
@@ -7,15 +7,19 @@ import 'package:kitawi/src/core/widget.dart';
 /// child fills the available space.
 
 class Expanded extends Widget {
-  Expanded({Key? key, this.child}) : super(key: key);
+  Expanded({Key? key, this.child, this.flex = 1}) : super(key: key);
 
+  /// The [child] contained by the [Expanded] widget.
   final Widget? child;
+
+  /// The flex factor to use for the child of the [Expanded] widget.
+  final int? flex;
 
   /// The [createElement] method creates the [DivElement] for the widget.
   @override
   Element createElement() {
     final element = Element.div();
-    element.style.flex = '1';
+    element.style.flex = flex.toString();
     if (child != null) {
       element.children.add(child!.render());
     }
