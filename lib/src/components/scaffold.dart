35e93e1065278e7efd821a60dfc37f3d71a28dfa
bryanbill <emobrya@gmail.com>
2023-02-23 11:27:23 +0300

    ft:scaffold widget - adds basic implementation of scaffold

--- lib/src/components/scaffold.dart
+++ lib/src/components/scaffold.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:kitawi/src/basic.dart';
 
 /// NOTE: This is a work in progress.
@@ -10,7 +8,7 @@ import 'package:kitawi/src/basic.dart';
 /// [floatingActionButton].
 /// The [Scaffold] class is a [Widget] class.
 class Scaffold extends StatelessWidget {
-  final Widget? body;
+  final Widget body;
   final Widget? navbar;
   final Widget? sidebar;
   final Widget? footer;
@@ -18,28 +16,12 @@ class Scaffold extends StatelessWidget {
   final Widget? floatingActionButton;
 
   Scaffold(
-      {this.body,
+      {required this.body,
       this.navbar,
       this.sidebar,
       this.footer,
       this.drawer,
       this.floatingActionButton});
-
-  /// The [createElement] method creates the [DivElement] for the widget.
-  @override
-  Element createElement() {
-    // TODO?: lookup this pattern
-    var wrapper = DivElement()
-      ..style.width = '100%'
-      ..style.height = '100%'
-      ..style.display = 'flex'
-      ..style.flexDirection = 'column'
-      ..style.justifyContent = 'space-between'
-      ..style.alignItems = 'stretch'
-      ..style.overflow = 'hidden';
-
-    return wrapper;
-  }
 
   @override
   Widget build() {
@@ -50,6 +32,20 @@ class Scaffold extends StatelessWidget {
         ),
         decoration: Decoration(
           color: Theme().mode == ThemeMode.light ? Colors.white : Colors.grey24,
+        ),
+        child: Row(
+          children: [
+            sidebar ?? Container(height: 0, width: 0),
+            Expanded(
+              child: Column(
+                children: [
+                  navbar ?? Container(),
+                  body,
+                  footer ?? Container(),
+                ],
+              ),
+            ),
+          ],
         ));
   }
 }
