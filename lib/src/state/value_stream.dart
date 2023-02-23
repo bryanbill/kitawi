060f453cf31023717173ab91da375649e2d1504a
bryanbill <emobrya@gmail.com>
2023-02-23 15:39:27 +0300

    fix:null safety - value of 'stream' can never be null

--- lib/src/state/value_stream.dart
+++ lib/src/state/value_stream.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 /// A [ValueStream] is a [Stream] that wraps a single value.
 
 class ValueStream<T> {
-  StreamController<T>? _controller;
+  late StreamController<T> _controller;
   T _value;
 
   ValueStream(this._value) {
@@ -12,16 +12,16 @@ class ValueStream<T> {
 
   /// The [stream] getter returns the stream of the [ValueStream].
 
-  Stream<T>? get stream => _controller?.stream;
+  Stream<T> get stream => _controller.stream;
 
   T get value => _value;
 
   set value(T newValue) {
     _value = newValue;
-    _controller?.add(newValue);
+    _controller.add(newValue);
   }
 
   void dispose() {
-    _controller?.close();
+    _controller.close();
   }
 }
