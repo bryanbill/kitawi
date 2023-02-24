14571103ef48d3b87ac102df688502827501c050
bryanbill <emobrya@gmail.com>
2023-02-24 11:32:08 +0300

    ft:image widget - adds lists of 'Actions' on the image widget

--- lib/src/components/image.dart
+++ lib/src/components/image.dart
@@ -31,6 +31,8 @@ class Image extends Widget {
 
   final BoxFit? fit;
 
+  /// The [Action]s to be performed on the image.
+  final List<Action>? actions;
   Image(this.src,
       {this.width,
       this.height,
@@ -38,29 +40,38 @@ class Image extends Widget {
       this.margin,
       this.decoration,
       this.alignment,
-      this.fit});
+      this.fit,
+      this.actions});
 
   /// The [createElement] method creates the [ImageElement] for the widget.
 
   @override
   Element createElement() {
-    return ImageElement(
-        src: src, width: width?.toInt(), height: height?.toInt())
-      ..style.width = width != null ? '${width}px' : '100%'
-      ..style.height = height != null ? '${height}px' : '100%'
-      ..style.maxWidth = width != null ? '${width}px' : '100%'
-      ..style.padding = '${padding ?? 0}px'
-      ..style.margin = '${margin ?? 0}px'
-      ..style.backgroundColor = decoration?.color?.rgba ?? 'inherit'
-      ..style.borderRadius = '${decoration?.border?.borderRadius ?? 0}px'
-      ..style.border =
-          '${decoration?.border?.width ?? 0}px solid ${decoration?.border?.color?.rgba}'
-      ..style.boxShadow =
-          decoration?.boxShadow?.map((e) => e.toString()).join(', ') ??
-              "inherit"
-      ..style.display = 'flex'
-      ..style.justifyContent = alignment?.x ?? 'inherit'
-      ..style.alignItems = alignment?.y ?? 'inherit'
-      ..style.objectFit = fit?.toString() ?? 'inherit';
+    var imageElement =
+        ImageElement(src: src, width: width?.toInt(), height: height?.toInt())
+          ..style.width = width != null ? '${width}px' : '100%'
+          ..style.height = height != null ? '${height}px' : '100%'
+          ..style.maxWidth = width != null ? '${width}px' : '100%'
+          ..style.padding = '${padding ?? 0}px'
+          ..style.margin = '${margin ?? 0}px'
+          ..style.backgroundColor = decoration?.color?.rgba ?? 'inherit'
+          ..style.borderRadius = '${decoration?.border?.borderRadius ?? 0}px'
+          ..style.border =
+              '${decoration?.border?.width ?? 0}px solid ${decoration?.border?.color?.rgba}'
+          ..style.boxShadow =
+              decoration?.boxShadow?.map((e) => e.toString()).join(', ') ??
+                  "inherit"
+          ..style.display = 'flex'
+          ..style.justifyContent = alignment?.x ?? 'inherit'
+          ..style.alignItems = alignment?.y ?? 'inherit'
+          ..style.objectFit = fit?.toString() ?? 'inherit';
+    if (actions != null) {
+      for (var action in actions!) {
+        imageElement.on[action.type].listen((event) {
+          action.callback(event);
+        });
+      }
+    }
+    return imageElement;
   }
 }
