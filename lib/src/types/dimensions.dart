7916c2fe9fa4298aad19c3153cc74517aef77444
bryanbill <emobrya@gmail.com>
2023-02-25 10:39:44 +0300

    docs:dimensions - updates the docs

--- lib/src/types/dimensions.dart
+++ lib/src/types/dimensions.dart
@@ -1,16 +1,84 @@
+/// A class that represents a CSS dimension value. It can be a number, a percentage, or a calculation.
+/// Example:
+/// ```dart
+/// print(Dimensions.of(100)); // "100px"
+/// print(Dimensions.full); // "100%"
+/// print(Dimensions.calc(from: Dimensions.of(100), inset: Dimensions.of(10))); // "calc(100px - 10px)"
+/// ```
+/// See also:
+/// * [Size], which is used for getting the size of the window.
+
 class Dimensions {
   final String _value;
 
+  /// Creates a [Dimensions] object with the given [value].
   const Dimensions._(this._value);
 
+  /// Returns a [Dimensions] object with the value "100%".
+  /// Example:
+  /// ```dart
+  /// print(Dimensions.full); // "100%"
+  /// ```
+  /// See also:
+  /// * [Dimensions.calc], which returns a [Dimensions] difference between two [Dimensions].
   static const Dimensions full = Dimensions._("100%");
 
+  /// Returns a [Dimensions] object with the value "auto".
+  /// Example:
+  /// ```dart
+  /// print(Dimensions.auto); // "auto"
+  /// ```
+  /// See also:
+  /// * [Dimensions.zero], which returns a [Dimensions] object with the value "0".
+  /// * [Dimensions.valueOf], which returns a string representation of this object.
+  /// * [Dimensions.toString], which returns a string representation of this object.
   static const Dimensions auto = Dimensions._("auto");
 
+  /// Returns a [Dimensions] object with the value "0".
+  /// Example:
+  /// ```dart
+  /// print(Dimensions.zero); // "0"
+  /// ```
   static const Dimensions zero = Dimensions._("0");
 
+  /// Calculates the difference between two [Dimensions] objects.
+  /// Example:
+  /// ```dart
+  /// print(Dimensions.calc(from: Dimensions.of(100), inset: Dimensions.of(10)));
+  /// // "calc(100px - 10px)"
+  /// ```
+  /// See also:
+  /// * [Dimensions.valueOf], which returns a string representation of this object.
+  /// * [Dimensions.toString], which returns a string representation of this object.
+  static Dimensions calc(
+          {required Dimensions from, required Dimensions inset}) =>
+      Dimensions._("calc($from - $inset)");
+
+  /// Returns a [Dimensions] object with the given [value] in pixels.
+  /// Example:
+  /// ```dart
+  /// print(Dimensions.of(100)); // "100px"
+  /// ```
+  /// See also:
+  /// * [Dimensions.valueOf], which returns a string representation of this object.
+  /// * [Dimensions.toString], which returns a string representation of this object.
   static Dimensions of(int value) => Dimensions._("${value}px");
 
+  /// Returns a [String] representation of this [Dimensions] object.
+  /// If the value is null, returns "auto".
+  ///
+  /// Example:
+  /// ```dart
+  /// Dimensions? width;
+  /// print(Dimensions.valueOf(width)); // "auto" => width is null
+  ///
+  /// width = Dimensions.of(100);
+  /// print(Dimensions.valueOf(width)); // "100px" => width is not null
+  /// ```
+  ///
+  /// See also:
+  ///
+  /// * [Dimensions.toString], which returns a string representation of this object.
   static String valueOf(Dimensions? value) => value?._value ?? "auto";
 
   @override
