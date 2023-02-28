88cba2ee857389c3867ea5a63d606644114ac81f
bryanbill <emobrya@gmail.com>
2023-02-28 23:28:44 +0300

    fix:sizedbox should always be exact height and width given

--- lib/src/components/sizedbox.dart
+++ lib/src/components/sizedbox.dart
@@ -5,17 +5,23 @@ import 'package:kitawi/src/basic.dart';
 /// A widget that is a specific size.
 /// If given a child, this widget forces its child to have a specific width and/or height (assuming values are permitted by this widget's parent).
 /// If not given a child, this widget will try to be as small as possible but still take up the allocated space given its width and height.
-class Sizedbox extends Widget {
+class SizedBox extends Widget {
   final Dimensions? width;
   final Dimensions? height;
   final Widget? child;
-  Sizedbox({this.width, this.height, this.child});
+  SizedBox({this.width, this.height, this.child});
 
   @override
   Element createElement() {
     return Container(
       width: width,
       height: height,
+      constraints: BoxConstraints(
+        minWidth: width,
+        minHeight: height,
+        maxHeight: height,
+        maxWidth: width,
+      ),
       child: child,
     ).render();
   }
