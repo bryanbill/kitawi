0d7cccbac76d2ba5bff8c7e45b3bea6740bb6583
bryanbill <emobrya@gmail.com>
2023-03-12 14:26:08 +0300

    ft:widget -  adds SideBar widget

---
+++ lib/src/components/sized_widgets/sidebar.dart
@@ -0,0 +1,40 @@
+import 'dart:html';
+
+import 'package:kitawi/src/basic.dart';
+
+class SideBar extends Widget {
+  final Widget? title;
+  final Widget? body;
+  final Widget? footer;
+  final Dimensions? width;
+  final Decoration? decoration;
+  final BorderRadius? borderRadius;
+
+  SideBar(
+      {Key? key,
+      this.title,
+      this.body,
+      this.footer,
+      this.width,
+      this.decoration,
+      this.borderRadius})
+      : super(key: key);
+
+  @override
+  Element createElement() {
+    var div = DivElement()
+      ..style.width = width?.toString() ?? '300px'
+      ..style.height = '100%'
+      ..style.display = 'flex'
+      ..style.flexDirection = 'column'
+      ..style.backgroundColor = decoration?.color?.rgba
+      ..style.borderRadius = borderRadius?.toString() ?? '0px'
+      ..style.boxShadow = decoration?.boxShadow?.toString() ?? 'none'
+      ..children = [
+        if (title != null) title!.createElement()..style.maxHeight = '64px',
+        if (body != null) body!.createElement()..style.flex = '1',
+        if (footer != null) footer!.createElement(),
+      ];
+    return div;
+  }
+}
