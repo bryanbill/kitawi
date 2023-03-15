0e90d25b9015076b1eadfb0e85c2880ce68a8f3d
bryanbill <emobrya@gmail.com>
2023-03-15 16:43:05 +0300

    ft: adds layout widget

---
+++ lib/src/components/layout.dart
@@ -0,0 +1,41 @@
+import 'dart:html';
+
+import 'package:kitawi/kitawi.dart';
+
+///
+abstract class Layout extends Widget {
+  Layout({Key? key}) : super(key: key);
+
+  late Element _element;
+
+  /// The [render] method creates the element for the widget.
+  @override
+  Element render() {
+    return StreamBuilder<Size?>(
+        initialData: windowSize.value,
+        stream: windowSize.stream,
+        errorWidgetBuilder: (error) =>
+            ErrorWidget(message: "Resize Error", details: error),
+        builder: (snap) => build()).render();
+  }
+
+  /// The [build] method creates the element for the widget.
+  /// It is an abstract method and must be implemented by the subclasses.
+  @override
+  Widget build();
+
+  @override
+  Element createElement() {
+    _element = build().render();
+    return _element;
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _element.remove();
+  }
+
+  @override
+  void init() {}
+}
