b7aa82ea89e8abd8734a44388ede2fc6be5e997b
bryanbill <emobrya@gmail.com>
2023-03-19 17:01:34 +0300

    ft:pageview - adds first pageview concept

--- lib/src/components/pageview.dart
+++ lib/src/components/pageview.dart
@@ -6,19 +6,23 @@ class PageView extends Widget {
   final List<Widget> children;
   final Axis scrollDirection;
   final double bounceExtent;
+  final ScrollPhysics? physics;
+  final PageController Function(
+      int initialOffset, ScrollMetrics metrics, Element element)? controller;
 
   PageView({
     Key? key,
     required this.children,
     this.scrollDirection = Axis.horizontal,
+    this.physics = const NeverScrollablePhysics(),
     this.bounceExtent = 0.2,
+    this.controller,
   }) : super(key: key);
 
   @override
   Element createElement() {
-    return DivElement()
+    var div = DivElement()
       ..style.display = (scrollDirection == Axis.horizontal) ? 'flex' : 'block'
-      ..style.overflow = 'auto'
       ..style.height = '100%'
       ..style.width = '100%'
       ..style.justifyContent =
@@ -26,8 +30,23 @@ class PageView extends Widget {
       ..style.alignItems =
           (scrollDirection == Axis.horizontal) ? 'center' : 'flex-start'
       ..children = children.map((e) => e.createElement()).toList()
-      ..classes.add('scrollable')
-      ..onScroll.listen((event) {});
+      ..classes.add('scrollable');
+
+    if (physics is NeverScrollablePhysics) {
+      div.style.overflow = 'hidden';
+    }
+
+    
+
+    if (physics is AlwaysScrollableScrollPhysics) {
+      div.style.overflow = 'scroll';
+
+      for (var element in div.children) {
+        element.classes.add('inner');
+      }
+    }
+
+    return div;
   }
 
   factory PageView.builder({
@@ -36,14 +55,31 @@ class PageView extends Widget {
     required Widget Function(int index) itemBuilder,
     Axis scrollDirection = Axis.horizontal,
     double bounceExtent = 0.2,
+    ScrollPhysics? physics = const NeverScrollablePhysics(),
   }) {
     return PageView(
-      key: key,
-      children: List.generate(itemCount, (index) {
-        return itemBuilder(index);
-      }),
-      scrollDirection: scrollDirection,
-      bounceExtent: bounceExtent,
-    );
+        key: key,
+        children: List.generate(itemCount, (index) {
+          return itemBuilder(index);
+        }),
+        scrollDirection: scrollDirection,
+        bounceExtent: bounceExtent,
+        physics: physics);
+  }
+}
+
+class PageController {
+  final int? initialScrollOffset;
+
+  final ScrollMetrics? metrics;
+
+  final Element? element;
+
+  PageController({this.metrics, this.initialScrollOffset = 0, this.element});
+
+  void jumpTo(double value) {
+    if (element != null) {
+      element!.scrollLeft = value.toInt();
+    }
   }
 }
---
+++ lib/src/components/widgets.md
@@ -0,0 +1,11 @@
+# Widgets Documentation
+
+This document describes the widgets that are provided by the Kitawi Library.
+
+## Overview
+
+The Kitawi Library provides a set of widgets that can be used to build the user interface of the application.
+
+## Built-in Widgets
+- [Frame](frame.dart)
+ - [Button](buttons/button.dart)
