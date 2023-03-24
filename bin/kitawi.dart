19fd1f42330b5c46f330325e913803570554d229
bryanbill <emobrya@gmail.com>
2023-03-24 22:47:36 +0300

    ft: updates the CLI

--- bin/kitawi.dart
+++ bin/kitawi.dart
@@ -1,52 +1,96 @@
-#!/usr/bin/env dart
-
 import 'dart:io';
+
 import 'package:args/args.dart';
+import 'dart:convert';
+import 'package:yaml/yaml.dart';
 
-/// This is a Dart program that uses the ArgParser library to parse
-/// command-line arguments. The program defines two commands
-/// (`create` and `run`) and two options (`port` and `name`).
-void main(List<String> args) async {
-  late ArgParser parser;
+void main(List<String> arguments) {
+  final parser = ArgParser()
+    ..addFlag('help',
+        abbr: 'h', negatable: false, help: 'Show usage information')
+    ..addCommand('run', _runCommandParser())
+    ..addCommand('create', _createCommandParser())
+    ..addCommand('build', _buildCommandParser());
+
+  ArgResults argResults;
   try {
-    parser = ArgParser()
-      ..addCommand('create')
-      ..addCommand('run')
-      ..addOption('port',
-          abbr: 'p',
-          help: 'The port to run the app on.',
-          valueHelp: 'port',
-          defaultsTo: '8080')
-      ..addOption("name",
-          help: 'The name of the app to create.',
-          valueHelp: 'name-of-app',
-          defaultsTo: 'kitawi-app');
+    argResults = parser.parse(arguments);
+  } on FormatException catch (e) {
+    print(e.message);
+    print('');
+    printUsage(parser);
+    return;
+  }
 
-    final results = parser.parse(args);
-    if (results.command == null) {
-      print(parser.usage);
-      exit(1);
-    } else {
-      if (results.command?.name == 'create') {
-        await create(results['name']);
-      }
+  if (argResults['help'] || argResults.command == null) {
+    printUsage(parser);
+    return;
+  }
 
-      if (results.command?.name == 'run') {
-        final port = int.parse(results['port']);
-        await run(port);
-      }
-    }
-  } catch (e) {
-    print(e.toString());
-    print(parser.usage);
-    exit(1);
+  final commandName = argResults.command!.name;
+  switch (commandName) {
+    case 'run':
+      _handleRunCommand(argResults.command!);
+      break;
+    case 'create':
+      _handleCreateCommand(argResults.command!);
+      break;
+    case 'build':
+      _handleBuildCommand(argResults.command!);
+      break;
+    default:
+      printUsage(parser);
+      break;
   }
 }
 
-/// This function creates a new Dart web app using the `dart create` command.
-/// It then adds the kitawi dependency to the pubspec.yaml file and runs
-/// `pub get` to install the kitawi dependency.
-Future<void> create(String name) async {
+void printUsage(ArgParser parser) {
+  print('Usage: kitawi <command> [arguments]\n');
+  print('Commands:');
+  print('  run    : Run a Kitawi application');
+  print('  create : Create a new Kitawi project');
+  print('  build  : Build a Kitawi project into an executable\n');
+  print(parser.usage);
+}
+
+ArgParser _runCommandParser() {
+  final parser = ArgParser();
+  parser.addOption('port', abbr: 'p', help: 'The port number to listen on');
+  return parser;
+}
+
+void _handleRunCommand(ArgResults args) async {
+  final port = args['port'] ?? 8080;
+
+  // Run webdev serve
+  final webdev = await Process.start(
+      'webdev', ['serve', 'web:$port', '--auto', 'refresh']);
+
+  // Pipe the output of the webdev command to stdout
+  webdev.stdout.listen((event) {
+    stdout.add(event);
+  });
+
+  // Pipe the error output of the webdev command to stderr
+  webdev.stderr.listen((event) {
+    stderr.add(event);
+  });
+
+  // Wait for the webdev command to finish
+  await webdev.exitCode;
+}
+
+ArgParser _createCommandParser() {
+  final parser = ArgParser();
+
+  parser.addOption('project_name',
+      abbr: 'n', help: 'The name of the project to create');
+  return parser;
+}
+
+void _handleCreateCommand(ArgResults args) async {
+  final projectName = args['project_name'];
+
   // Enable webdev
   final webdev =
       await Process.start('dart', ['pub', 'global', 'activate', 'webdev']);
@@ -65,7 +109,8 @@ Future<void> create(String name) async {
   await webdev.exitCode;
 
   // dart create web app
-  final create = await Process.start('dart', ['create', '-t' 'web', name]);
+  final create =
+      await Process.start('dart', ['create', '-t' 'web', projectName]);
 
   // Pipe the output of the create command to stdout
   create.stdout.listen((event) {
@@ -81,10 +126,10 @@ Future<void> create(String name) async {
   await create.exitCode;
 
   // scaffold the app
-  scaffold(name);
+  scaffold(projectName);
 
   // Change directory to the newly created app
-  Directory.current = name;
+  Directory.current = projectName;
 
   // Add kitawi: any as a dependency in pubspec.yaml
   final pubspec = File('pubspec.yaml');
@@ -92,12 +137,31 @@ Future<void> create(String name) async {
   // Read the contents of the pubspec.yaml file
   final contents = await pubspec.readAsString();
 
-  // get the version of kitawi from contents
-  // final version = contents.split('version: ')[1].split('\n')[0];
-
   // Add kitawi: any as a dependency
-  final updatedContents =
-      contents.replaceFirst('dependencies:', 'dependencies:\n  kitawi: ^0.0.4');
+  var updatedContents =
+      contents.replaceFirst('dependencies:', 'dependencies:\n  kitawi: ^0.0.5');
+
+  final manifest = '''
+
+manifest: 
+  - name: $projectName
+  - short_name: $projectName
+  - id: /#/
+  - start_url: /#/
+  - display: standalone
+  - background_color: "#ffffff"
+  - theme_color: "#4DB6AC"
+  - orientation: portrait-primary
+  - icons:
+      - src: /assets/images/icon.png
+        sizes: 192x192
+        type: image/png
+''';
+
+// add the manifest contents to the pubspec.yaml file
+  updatedContents = contents.replaceFirst(
+          'dependencies:', 'dependencies:\n  kitawi: ^0.0.6') +
+      manifest;
 
   // Write the updated contents to the pubspec.yaml file
   await pubspec.writeAsString(updatedContents);
@@ -117,32 +181,95 @@ Future<void> create(String name) async {
 
   // Wait for the pub get command to finish
   await pubGet.exitCode;
+  ;
 }
 
-/// This function runs the web app using the `webdev serve` command.
-/// It takes the port to run the app on as an argument.
-/// The default port is 8080.
-Future<void> run(int port) async {
-  // Run webdev serve
-  final webdev = await Process.start(
-      'webdev', ['serve', 'web:$port', '--auto', 'refresh']);
+ArgParser _buildCommandParser() {
+  final parser = ArgParser();
 
-  // Pipe the output of the webdev command to stdout
-  webdev.stdout.listen((event) {
-    stdout.add(event);
-  });
+  parser.addOption('no-pwa',
+      help: 'Build a Kitawi project without PWA support');
 
-  // Pipe the error output of the webdev command to stderr
-  webdev.stderr.listen((event) {
-    stderr.add(event);
-  });
-
-  // Wait for the webdev command to finish
-  await webdev.exitCode;
+  return parser;
 }
 
-/// This function is used to scaffold a new kitawi app.
-/// It takes the name of the app as an argument.
+void _handleBuildCommand(ArgResults args) async {
+  final noPwa = args['no-pwa'] ?? false;
+  if (noPwa) {
+    var process = await Process.start('webdev', ['build']);
+
+    // Pipe the output of the webdev command to stdout
+    process.stdout.listen((event) {
+      stdout.add(event);
+    });
+
+    // Pipe the error output of the webdev command to stderr
+    process.stderr.listen((event) {
+      stderr.add(event);
+    });
+
+    // Wait for the webdev command to finish
+    await process.exitCode;
+  } else {
+    var process = await Process.start('webdev', ['build']);
+
+    // Pipe the output of the webdev command to stdout
+    process.stdout.listen((event) {
+      stdout.add(event);
+    });
+
+    // Pipe the error output of the webdev command to stderr
+    process.stderr.listen((event) {
+      stderr.add(event);
+    });
+
+    // Wait for the webdev command to finish
+    await process.exitCode;
+
+    // read the pubscpec.yaml file and get the entries of manifest
+    final pubspec = File('pubspec.yaml');
+
+    // Read the contents of the pubspec.yaml file
+    final contents = await pubspec.readAsString();
+
+    //load the yaml file
+    final yaml = loadYaml(contents);
+
+    // get the manifest entries
+    final manifest = yaml['manifest'];
+
+    Map<String, dynamic> manifestMap = {};
+
+    // convert the manifest entries to a map
+    for (final entry in manifest) {
+      manifestMap[entry.keys.first] = entry.values.first;
+    }
+
+    // create the manifest.json file
+    final manifestFile = File('build/manifest.json');
+
+    // write the contents of the manifest.json file
+    await manifestFile.writeAsString(jsonEncode(manifestMap));
+
+    // add the link to the manifest.json file in the index.html file
+    final indexFile = File('build/index.html');
+
+    // read the contents of the index.html file
+    final indexContents = await indexFile.readAsString();
+
+    // add the link to the manifest.json file in the index.html file
+
+    final updatedIndexContents = indexContents.replaceFirst('</head>', '''
+  <link rel="manifest" href="manifest.json">
+</head>
+
+        ''');
+
+    // write the updated contents of the index.html file
+    await indexFile.writeAsString(updatedIndexContents);
+  }
+}
+
 void scaffold(String name) {
   try {
     // delete the index.html, style.css, and main.dart files in the <name>/web directory
@@ -152,6 +279,44 @@ void scaffold(String name) {
     for (final file in deleteResources) {
       file.deleteSync();
     }
+    // create the css directory in <name>/web/assets directory
+    final cssDir = Directory('$name/web/assets/css');
+    cssDir.createSync(recursive: true);
+
+    // create the main.css in <name>/web/assets/css directory
+    final mainCss = File('$name/web/assets/css/main.css');
+
+    // Write the contents of the main.css file
+    mainCss.writeAsStringSync('''
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+  color: #333;
+  background-color: #fff;
+}
+
+ /* Hide scrollbar for all modern browsers except IE */
+.scrollable::-webkit-scrollbar {
+  width: 0;
+  height: 0;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.scrollable {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+
+/* Hide scrollbar thumb */
+.scrollable::-webkit-scrollbar-thumb {
+  background-color: rgba(0, 0, 0, 0);
+  width: 0;
+  height: 0;
+}
+
+''');
 
     // create a new index.html file in the <name>/web directory
     final index = File('$name/web/index.html');
@@ -160,37 +325,23 @@ void scaffold(String name) {
     index.writeAsStringSync('''
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+  <head>    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Kitawi is a responsive UI framework for building modern web applications.">
     <meta name="keywords" content="kitawi, ui framework, web development">
+    <meta name="author" content="Kitawi">
+    <meta name="theme-color" content="#4DB6AC">
+    
     <title>$name</title>
-    <style>
-      /* Hide scrollbar for all modern browsers except IE */
-      .scrollable::-webkit-scrollbar {
-        width: 0;
-        height: 0;
-      }
-
-      /* Hide scrollbar for IE, Edge and Firefox */
-      .scrollable {
-        -ms-overflow-style: none; /* IE and Edge */
-        scrollbar-width: none; /* Firefox */
-      }
-
-      /* Hide scrollbar thumb */
-      .scrollable::-webkit-scrollbar-thumb {
-        background-color: rgba(0, 0, 0, 0);
-        width: 0;
-        height: 0;
-      }
-    </style>
+    <link rel="stylesheet" href="assets/css/main.css">
   </head>
+
   <body>
     <div id="root"></div>
-    <script src="main.dart.js"></script>
+    <script src="main.dart.js" defer></script>
   </body>
+
 </html>
 ''');
 
@@ -201,87 +352,25 @@ void scaffold(String name) {
     main.writeAsStringSync('''
 import 'package:kitawi/kitawi.dart';
 
-/// This is the entry point of the app.
 void main() {
-
-  /// This is the root element of the app.
-  /// [run] takes id of the root element as an argument.
-  /// The default id is 'root'.
-  start(
-    () => run(MyApp(), id: 'output'),
+  run(
+    [
+      Route(
+        builder: (args) => App(), 
+        path: "/", 
+        name: "App"
+      ),
+    ],
+    id: 'root', // This is the id of element that will contain your app
+    env: Environment.dev,
   );
 }
 
-class Clock {
-  final int? hour;
-  final int? minute;
-  final int? second;
+class App extends Layout {
 
-  Clock({this.hour, this.minute, this.second});
-
-  @override
-  String toString() {
-    return '\$hour:\$minute:\$second';
-  }
-}
-
-/// This is the main widget of the app.
-class MyApp extends Kitawi {
-
-  /// This is a stream of [Clock] objects.
-  /// It is used to update the time every second.
-  Stream<Clock> clock() async* {
-    int hour = 0;
-    int minute = 0;
-    int second = 0;
-    while (true) {
-      await Future.delayed(Duration(seconds: 1));
-
-      if (second - 1 == 60) {
-        second = 0;
-        minute++;
-      }
-
-      if (minute - 1 == 60) {
-        minute = 0;
-        hour++;
-      }
-
-      if (hour - 1 == 24) {
-        hour = 0;
-      }
-      yield Clock(hour: hour, minute: minute, second: second++);
-    }
-  }
-
-/// This is the build method of the app.
-/// It returns a [Scaffold] widget.
   @override
   Widget build() {
-    return Scaffold(
-      body: Container(
-        height: 100.percent,
-        width: 100.percent,
-        alignment: Alignment.center,
-        decoration: Decoration(
-          color: Theme.mode == ThemeMode.dark ? Colors.white : Colors.black,
-        ),
-        child: StreamBuilder<Clock>(
-          stream: clock(),
-          initialData: Clock(hour: 0, minute: 0, second: 0),
-          builder: (snapshot) {
-            return Text(
-              snapshot.data.toString(),
-              style: TextStyle(fontSize: 30),
-            );
-          },
-          errorWidgetBuilder: (error) => Text(
-            error.toString(),
-            style: TextStyle(fontSize: 30, color: Colors.red),
-          ),
-        ),
-      ),
-    );
+    return Center(child: Text("Hello World!"));
   }
 }
 ''');
