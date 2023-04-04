04911d86d48974b41a8f15b1cff7feef24bf9a30
bryanbill <emobrya@gmail.com>
2023-04-04 18:37:52 +0300

    ft: Text widgets

--- bin/kitawi.dart
+++ bin/kitawi.dart
@@ -1,9 +1,6 @@
 import 'dart:io';
 
 import 'package:args/args.dart';
-import 'dart:convert';
-
-import 'package:yaml/yaml.dart';
 
 void main(List<String> arguments) {
   final parser = ArgParser()
@@ -63,280 +60,72 @@ ArgParser _runCommandParser() {
 void _handleRunCommand(ArgResults args) async {
   final port = args['port'] ?? 8080;
 
-  // Run webdev serve
   final webdev = await Process.start(
       'webdev', ['serve', 'web:$port', '--auto', 'refresh']);
 
-  // Pipe the output of the webdev command to stdout
   webdev.stdout.listen((event) {
     stdout.add(event);
   });
 
-  // Pipe the error output of the webdev command to stderr
   webdev.stderr.listen((event) {
     stderr.add(event);
   });
 
-  // Wait for the webdev command to finish
   await webdev.exitCode;
 }
 
 ArgParser _createCommandParser() {
   final parser = ArgParser();
-
-  parser.addOption('project_name',
-      abbr: 'n', help: 'The name of the project to create');
   return parser;
 }
 
 void _handleCreateCommand(ArgResults args) async {
-  final projectName = args['project_name'];
+  final appName = args.rest.first;
 
-  // Enable webdev
-  final webdev =
-      await Process.start('dart', ['pub', 'global', 'activate', 'webdev']);
+  final git = await Process.start('git', [
+    'clone',
+    '',
+    appName,
+  ]);
 
-  // Pipe the output of the webdev command to stdout
-  webdev.stdout.listen((event) {
+  git.stdout.listen((event) {
     stdout.add(event);
   });
-
-  // Pipe the error output of the webdev command to stderr
-  webdev.stderr.listen((event) {
+  git.stderr.listen((event) {
     stderr.add(event);
   });
 
-  // Wait for the webdev command to finish
-  await webdev.exitCode;
+  await git.exitCode;
 
-  // dart create web app
-  final create =
-      await Process.start('dart', ['create', '-t' 'web', projectName]);
+  final rm = await Process.start('rm', ['-rf', '$appName/.git']);
 
-  // Pipe the output of the create command to stdout
-  create.stdout.listen((event) {
+  rm.stdout.listen((event) {
     stdout.add(event);
   });
 
-  // Pipe the error output of the create command to stderr
-  create.stderr.listen((event) {
+  rm.stderr.listen((event) {
     stderr.add(event);
   });
 
-  // Wait for the create command to finish
-  await create.exitCode;
-
-  // scaffold the app
-  scaffold(projectName);
-
-  // Run pub get to install the kitawi dependency
-  final pubGet = await Process.start('dart', ['pub', 'add', 'kitawi']);
-
-  // Pipe the output of the pub get command to stdout
-  pubGet.stdout.listen((event) {
-    stdout.add(event);
-  });
-
-  // Pipe the error output of the pub get command to stderr
-  pubGet.stderr.listen((event) {
-    stderr.add(event);
-  });
-
-  // Wait for the pub get command to finish
-  await pubGet.exitCode;
+  await rm.exitCode;
 }
 
 ArgParser _buildCommandParser() {
   final parser = ArgParser();
 
-  parser.addOption('no-pwa',
-      help: 'Build a Kitawi project without PWA support');
-
   return parser;
 }
 
 void _handleBuildCommand(ArgResults args) async {
-  final noPwa = args['no-pwa'] ?? false;
-  if (noPwa) {
-    var process = await Process.start('webdev', ['build']);
+  var process = await Process.start('webdev', ['build']);
 
-    // Pipe the output of the webdev command to stdout
-    process.stdout.listen((event) {
-      stdout.add(event);
-    });
+  process.stdout.listen((event) {
+    stdout.add(event);
+  });
 
-    // Pipe the error output of the webdev command to stderr
-    process.stderr.listen((event) {
-      stderr.add(event);
-    });
+  process.stderr.listen((event) {
+    stderr.add(event);
+  });
 
-    // Wait for the webdev command to finish
-    await process.exitCode;
-  } else {
-    var process = await Process.start('webdev', ['build']);
-
-    // Pipe the output of the webdev command to stdout
-    process.stdout.listen((event) {
-      stdout.add(event);
-    });
-
-    // Pipe the error output of the webdev command to stderr
-    process.stderr.listen((event) {
-      stderr.add(event);
-    });
-
-    // Wait for the webdev command to finish
-    await process.exitCode;
-
-    // read the pubscpec.yaml file and get the entries of manifest
-    final pubspec = File('pubspec.yaml');
-
-    // Read the contents of the pubspec.yaml file
-    final contents = await pubspec.readAsString();
-
-    //load the yaml file
-    final yaml = loadYaml(contents);
-
-    // get the manifest entries
-    final manifest = yaml['manifest'];
-
-    Map<String, dynamic> manifestMap = {};
-
-    // convert the manifest entries to a map
-    for (final entry in manifest) {
-      manifestMap[entry.keys.first] = entry.values.first;
-    }
-
-    // create the manifest.json file
-    final manifestFile = File('build/manifest.json');
-
-    // write the contents of the manifest.json file
-    await manifestFile.writeAsString(jsonEncode(manifestMap));
-
-    // add the link to the manifest.json file in the index.html file
-    final indexFile = File('build/index.html');
-
-    // read the contents of the index.html file
-    final indexContents = await indexFile.readAsString();
-
-    // add the link to the manifest.json file in the index.html file
-
-    final updatedIndexContents = indexContents.replaceFirst('</head>', '''
-  <link rel="manifest" href="manifest.json">
-</head>
-
-        ''');
-
-    // write the updated contents of the index.html file
-    await indexFile.writeAsString(updatedIndexContents);
-  }
-}
-
-void scaffold(String name) {
-  try {
-    // delete the index.html, style.css, and main.dart files in the <name>/web directory
-
-    final deleteResources = Directory('$name/web').listSync();
-
-    for (final file in deleteResources) {
-      file.deleteSync();
-    }
-    // create the css directory in <name>/web/assets directory
-    final cssDir = Directory('$name/web/assets/css');
-    cssDir.createSync(recursive: true);
-
-    // create the main.css in <name>/web/assets/css directory
-    final mainCss = File('$name/web/assets/css/main.css');
-
-    // Write the contents of the main.css file
-    mainCss.writeAsStringSync('''
-body {
-  margin: 0;
-  padding: 0;
-  font-family: 'Roboto', sans-serif;
-  font-size: 16px;
-  color: #333;
-  background-color: #fff;
-}
-
- /* Hide scrollbar for all modern browsers except IE */
-.scrollable::-webkit-scrollbar {
-  width: 0;
-  height: 0;
-}
-
-/* Hide scrollbar for IE, Edge and Firefox */
-.scrollable {
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-}
-
-/* Hide scrollbar thumb */
-.scrollable::-webkit-scrollbar-thumb {
-  background-color: rgba(0, 0, 0, 0);
-  width: 0;
-  height: 0;
-}
-
-''');
-
-    // create a new index.html file in the <name>/web directory
-    final index = File('$name/web/index.html');
-
-    // Write the contents of the index.html file
-    index.writeAsStringSync('''
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Kitawi is a responsive UI framework for building modern web applications.">
-    <meta name="keywords" content="kitawi, ui framework, web development">
-    <meta name="author" content="Kitawi">
-    <meta name="theme-color" content="#4DB6AC">
-
-    <title>$name</title>
-    <link rel="stylesheet" href="assets/css/main.css">
-  </head>
-
-  <body>
-    <div id="root"></div>
-    <script src="main.dart.js" defer></script>
-  </body>
-
-</html>
-''');
-
-// create new main.dart file in the <name>/web directory
-    final main = File('$name/web/main.dart');
-
-    // Write the contents of the main.dart file
-    main.writeAsStringSync('''
-import 'package:kitawi/kitawi.dart';
-
-void main() {
-  run(
-    [
-      Route(
-        builder: (args) => App(),
-        path: "/",
-        name: "App"
-      ),
-    ],
-    id: 'root', // This is the id of element that will contain your app
-    env: Environment.dev,
-  );
-}
-
-class App extends Layout {
-
-  @override
-  Widget build() {
-    return Center(child: Text("Hello World!"));
-  }
-}
-''');
-  } catch (e) {
-    print(e.toString());
-  }
+  await process.exitCode;
 }
