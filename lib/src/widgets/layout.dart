75ee43b6a008da3c93024fed69445bed86d4e91a
bryanbill <emobrya@gmail.com>
2023-04-21 13:25:36 +0300

    ft:makes 'Layout' mutable

--- lib/src/widgets/layout.dart
+++ lib/src/widgets/layout.dart
@@ -2,34 +2,34 @@ import 'dart:html';
 
 import 'package:kitawi/widgets.dart';
 
+/// Represents a widget with mutable state.
+
 abstract class Layout extends Widget {
+  Element? _element;
+
   Layout({Key? key}) : super(key: key) {
     init();
   }
 
-  Element? _element;
+  void init() {}
 
-  /// The [render] method creates the element for the widget.
-  @override
-  Element render() {
-    return build().render();
-  }
-
-  @override
   Widget build();
 
   @override
   Element createElement() {
-    _element = build().render();
+    _element = build().createElement()..id = key.toString();
     return _element!;
   }
 
-  void init() {}
-
-  @override
   void dispose() {
-    _element?.remove();
+    _element!.remove();
     _element = null;
-    super.dispose();
   }
+
+  void setState(void Function() fn) {
+    fn();
+    if (build().canUpdate(this)) render(build(), _element);
+  }
+
+  bool get mounted => _element != null;
 }
--- lib/src/widgets/widget.dart
+++ lib/src/widgets/widget.dart
@@ -1,38 +1,22 @@
-import 'dart:html' as html;
+import 'dart:html';
+
 import 'package:kitawi/widgets.dart';
-import 'package:kitawi/src/core/key.dart';
 
-/// A widget that can be built and rendered to HTML.
+/// Base class for all widgets.
 ///
-/// This widget is the base class for all widgets in the framework. It provides
-/// a `render` method that can be used to generate the HTML element for the
-/// widget. It also provides a `build` method that is called when the state of
-/// the widget changes, so that the widget can rebuild itself.
+/// A widget is an immutable description of part of a user interface.
+
 abstract class Widget {
-  Key? key;
+  /// The key that uniquely identifies this widget.
+  final Key? key;
 
-  html.Element? _element;
+  const Widget({this.key});
 
-  Widget({this.key});
+  /// Creates an element that represents this widget in the render tree.
+  Element createElement();
 
-  /// The [render] method creates the element for the widget.
-  html.Element render() {
-    _element ??= createElement();
-    return _element!;
-  }
-
-  /// The [createElement] method creates the element for the widget.
-  /// It is an abstract method and must be implemented by the subclasses.
-  html.Element createElement();
-
-  void build() {
-    _element ??= createElement();
-  }
-
-  /// The [dispose] method is called when the widget is removed from the tree.
-  /// It is an abstract method and must be implemented by the subclasses.
-  void dispose() {
-    _element?.remove();
-    _element = null;
+  /// Returns true if this widget can be updated with the oldWidget.
+  bool canUpdate(Widget oldWidget) {
+    return oldWidget.key == key;
   }
 }
